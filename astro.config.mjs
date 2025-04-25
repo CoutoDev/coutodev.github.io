8a6eddb0e1731d5ff3f487f02357786292970a6a
CoutoDev <c.svitor@yahoo.com.br>
2025-04-24 23:24:33 -0300

    feat: update styles to use tailwind and change posts types

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://coutodev.github.io',
-    integrations: [mdx(), sitemap(), react()],
+  site: 'https://coutodev.github.io',
+  integrations: [mdx(), sitemap(), react()],
+
+  redirects: {
+    '/': '/blog',
+  },
+
+  vite: {
+    plugins: [tailwind()]
+  }
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'atkinson': ['Atkinson', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+      },
+      colors: {
+        green: {
+          400: '#59F22E',
+          500: '#4AD926',
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')({
+      className: 'prose',
+      modifiers: ['invert', 'green'],
+    }),
+  ],
+} 
